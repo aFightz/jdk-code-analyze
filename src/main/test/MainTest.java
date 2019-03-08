42de314b1f81878f9f2ddbf0d7fc96180450d4e5
huangjunlong <huang.junlong@intellif.com>
2019-03-08 14:28:44 +0800

    hashmap源码分析

---
+++ src/main/test/MainTest.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+        Map<Integer , Integer> map = new HashMap();
+    }
+}
